9e5d424d67d0284c61e771d757b6b6766079b48d
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2022-03-15 12:32:22 +0100

    Add an example link

--- src/pages/news/index.tsx
+++ src/pages/news/index.tsx
@@ -1,4 +1,14 @@
+import Link from 'next/link';
+import { Fragment } from 'react';
+
 const NewsPage = () => {
-  return <h1>The News Page</h1>;
+  return (
+    <Fragment>
+      <h1>The News Page</h1>
+      <ul>
+        <Link href='/news/example'>Example</Link>
+      </ul>
+    </Fragment>
+  );
 };
 export default NewsPage;
