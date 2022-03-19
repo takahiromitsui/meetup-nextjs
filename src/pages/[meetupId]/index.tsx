5a0e0113e5f66d52e80550843ce12ff127283896
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2022-03-19 11:39:29 +0100

    feat: preload all detail pages

--- src/pages/[meetupId]/index.tsx
+++ src/pages/[meetupId]/index.tsx
@@ -1,6 +1,8 @@
+import { MongoClient } from 'mongodb';
 import { GetStaticProps } from 'next';
 
 import MeetupDetail from '@/components/meetups/MeetupDetail';
+
 const MeetupDetails = () => (
   <MeetupDetail
     image='https://www.tripsavvy.com/thmb/VvaRvLCcafdpJcarRCtVfoDEoPM=/5323x3549/filters:fill(auto,1)/MuseumIsland-0fd599d4eccb4a3b9da13ac2af0f1325.jpg'
@@ -12,22 +14,22 @@ const MeetupDetails = () => (
 export default MeetupDetails;
 
 export const getStaticPaths = async () => {
+  // fetch data
+  const client = await MongoClient.connect(
+    `mongodb+srv://${process.env.USER_NAME}:${process.env.DATABASE_PASSWORD}@cluster0.vnelg.mongodb.net/meetups?retryWrites=true&w=majority`
+  );
+  const db = client.db();
+  const meetupsCollection = db.collection('meetups');
+  // fetch all ids
+  const meetups = await meetupsCollection
+    .find({}, { projection: { _id: 1 } })
+    .toArray();
+
   return {
-    fallback: false, //only m1 and m2 will be preloaded
-    paths: [
-      {
-        params: {
-          //hardcoded now
-          meetupId: 'm1',
-        },
-      },
-      {
-        params: {
-          //hardcoded now
-          meetupId: 'm2',
-        },
-      },
-    ],
+    fallback: false,
+    paths: meetups.map((meetup) => ({
+      params: { meetupId: meetup._id.toString() },
+    })),
   };
 };
 
